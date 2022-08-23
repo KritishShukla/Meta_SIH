b83b8525e313c993d1da395721bc4da18842d551
KritishShukla <kritish.shukla0007@gmail.com>
2022-08-23 15:34:44 +0530

    sih meta pushed

---
+++ Assets/scene_change.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_change : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Auditorium");
+        Debug.Log("hi");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+   
+}
